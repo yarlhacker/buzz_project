3aa30175ed6faaf2666317cdb9e59f2dce39d29f
yarlhacker <ismael.developper@gmail.com>
2021-05-25 12:13:28 +0000

    first commit

--- purplebuzz/settings.py
+++ purplebuzz/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import  os 
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-=e3_jlo6hx-jhbgdok*ctg_*%t3rmnmxlhhg+*k%$pljp7_ie*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['buzzprojet.herokuapp.com  ']
 
 
 # Application definition
@@ -122,6 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
 STATIC_ROOT = BASE_DIR / 'static_cdn'
 
 MEDIA_URL = '/media/'
@@ -135,3 +137,4 @@ STATICFILES_DIRS = [BASE_DIR /'static']
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
